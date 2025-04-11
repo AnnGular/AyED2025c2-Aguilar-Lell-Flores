7857c654bae4c2970e51343e1d68782c6430b604
valentinafloresssssss <valentinaaaflores09@gmail.com>
2025-04-10 21:03:56 -0300

    primer modificacion

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,3 +1,52 @@
-# módulo para organizar funciones o clases utilizadas en nuestro proyecto
-# Crear tantos módulos como sea necesario para organizar el código
-#hola
+# modulo1.py
+
+def burbuja(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+    return lista
+
+
+def quicksort(lista):
+    if len(lista) <= 1:
+        return lista
+    pivote = lista[len(lista) // 2]
+    menores = [x for x in lista if x < pivote]
+    iguales = [x for x in lista if x == pivote]
+    mayores = [x for x in lista if x > pivote]
+    return quicksort(menores) + iguales + quicksort(mayores)
+
+
+def counting_sort(lista, exp):
+    n = len(lista)
+    salida = [0] * n
+    cuenta = [0] * 10
+
+    for i in range(n):
+        indice = (lista[i] // exp) % 10
+        cuenta[indice] += 1
+
+    for i in range(1, 10):
+        cuenta[i] += cuenta[i - 1]
+
+    i = n - 1
+    while i >= 0:
+        indice = (lista[i] // exp) % 10
+        salida[cuenta[indice] - 1] = lista[i]
+        cuenta[indice] -= 1
+        i -= 1
+
+    for i in range(n):
+        lista[i] = salida[i]
+
+
+def radixsort(lista):
+    maximo = max(lista)
+    exp = 1
+    while maximo // exp > 0:
+        counting_sort(lista, exp)
+        exp *= 10
+    return lista
+
--- TrabajoPractico_1/proyecto_1/tests/test_modulo1.py
+++ TrabajoPractico_1/proyecto_1/tests/test_modulo1.py
@@ -1,1 +1,36 @@
 # Archivo de test para realizar pruebas unitarias del modulo1
+from modules.modulo1 import burbuja, quicksort, radixsort
+
+def test_burbuja():
+    lista = [5, 3, 1, 4, 2]
+    esperado = [1, 2, 3, 4, 5]
+    resultado = burbuja(lista.copy())
+    if resultado == esperado:
+        print("burbuja(): OK")
+    else:
+        print("burbuja(): ERROR")
+
+def test_quicksort():
+    lista = [5, 3, 1, 4, 2]
+    esperado = [1, 2, 3, 4, 5]
+    resultado = quicksort(lista.copy())
+    if resultado == esperado:
+        print("quicksort(): OK")
+    else:
+        print("quicksort(): ERROR")
+
+def test_radixsort():
+    lista = [5, 3, 1, 4, 2]
+    esperado = [1, 2, 3, 4, 5]
+    resultado = radixsort(lista.copy())
+    if resultado == esperado:
+        print("radixsort(): OK")
+    else:
+        print("radixsort(): ERROR")
+
+
+if __name__ == "__main__":
+    test_burbuja()
+    test_quicksort()
+    test_radixsort()
+
